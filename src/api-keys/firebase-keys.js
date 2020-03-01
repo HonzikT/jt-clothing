7d87b02f9d7b37cf0c8eae8a4b4f222ded3e2204
HonzikT <truxajan@gmail.com>
2020-03-01 11:41:27 +0100

    added api keys because of heroku build

---
+++ src/api-keys/firebase-keys.js
@@ -0,0 +1,5 @@
+const firebaseKeys = {
+  api_key: "AIzaSyDCn4Pi5YKcfNwi9KcwXEXVWZtlTPSYTyE"
+}
+
+export default firebaseKeys;
---
+++ src/api-keys/stripe-keys.js
@@ -0,0 +1,5 @@
+const stripeKeys = {
+  PUBLISHABLE_KEY: 'pk_test_c6BZwH2j78A9bw1OcABsn8M6008xV2q6Pk'
+}
+
+export default stripeKeys;
